e773170140699b692930c0f4511f93ea3e41a9b3
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-30 14:58:53 +0900

    05/30-14:58

--- public/js/main.js
+++ public/js/main.js
@@ -5,11 +5,10 @@ const app = new Vue({
     data: {
         qaGroup: 0,
         qas: [
-            { value: false , text: '内定式の出欠について 回答ください' },
+            { value: false , text: '内定式には出席しましたか' },
             { value: false , text: '内定式の感想について 回答ください' },
             { value: false , text: '独身寮への入寮を希望しますか' },
             { value: false , text: '新人研修座談会（入社前研修）への出欠について回答ください' },
-            { value: false , text: '災害情報について' },
         ],
         setq: [
             { text: '質問1' },
@@ -24,9 +23,33 @@ const app = new Vue({
 
     methods: {
         postAns () {
-            fetch('https://testforlife.herokuapp.com/', {
+            console.log(Object(this.qas).length);
+            let postdata = {
+                "app": 6,
+                "record": {
+                    "aImpressions": {
+                        "value": "入力ああああ"
+                    },
+                    "attend": {
+                        "value": 1
+                    },
+                    "dom": {
+                        "value": 0
+                    },
+                    "attend_for_new_comer": {
+                        "value": 1
+                    }
+                }
+            };
+            /*-- 時間あったら4つ以上の質問にも対応できるように
+            for (let i=0; i<Object(this.qas).length; i++) {
+                postdata = 
+            }
+            */
+            fetch('https://first-project-9474-dev.twil.io/defalut_question', {
                 method: 'post',
-                body: JSON.stringify(this.qas)
+                //body: JSON.stringify(this.qas)
+                body: JSON.stringify(postdata)
             })
             .then(response => {
                 console.log(response);
