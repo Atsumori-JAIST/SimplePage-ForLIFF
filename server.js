f5752891dd4b8f623e4fd1cd4e034312ecb53440
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-30 13:12:08 +0900

    05/30-13:12

--- server.js
+++ server.js
@@ -52,11 +52,23 @@ http.createServer(function (request, response) {
 
     if (request.method === 'POST'){
         var data = '';
+        const dburl = 'https://first-project-9474-dev.twil.io/hello-world';
         request.on('data', function(chunk) { data += chunk })
             .on('end', function() {
-                console.log(data);
-                console.log(data[0]);
-                console.log(data[1]);
+                //console.log(data);
+                setFullfill(false);
+                axios.post(dburl,
+                    {
+                        "app": 3,
+                            "record": {
+                                "question": {
+                                    "value": "Question Test"
+                                },
+                                "answer": {
+                                    "value": "Answer Test"
+                                }
+                            }
+                    })
             })
     }
 
